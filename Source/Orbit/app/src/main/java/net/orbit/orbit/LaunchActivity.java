421d93934b163cd4ea5752169c7638e5c9add7fb
Kevin Stanley <kevin.patrick.stanley@gmail.com>
2017-10-04 20:27:52 -0500

    OR-33 - Initial creation of app.  This includes the default login activity provided by Android Studio and a custom splash screen created my Kevin.

---
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/LaunchActivity.java
@@ -0,0 +1,42 @@
+package net.orbit.orbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new BackgroundTask().execute();
+
+    }
+
+    public class BackgroundTask extends AsyncTask {
+        Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            startActivity(this.intent);
+            finish();
+        }
+    }
+
+}
