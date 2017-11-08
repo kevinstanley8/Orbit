af6666b19debf1880d85382e9370af0ae2381dc0
Brock Tubre <brock.tubre@csra.com>
2017-11-08 08:53:13 -0600

    Removed AddTeacher button

--- Source/Orbit/app/src/main/java/net/orbit/orbit/HomeActivity.java
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/HomeActivity.java
@@ -38,16 +38,17 @@ public class HomeActivity extends BaseActivity {
         String apiUrl = propertiesService.getProperty(this,"orbit.api.url");
         orbitRestClient.setBaseUrl(apiUrl);
 
-        final Button button = (Button) findViewById(R.id.addTeacher);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent addTeacherIntent = new Intent(HomeActivity.this, AddTeacherActivity.class);
-                HomeActivity.this.startActivity(addTeacherIntent);
-            }
-        });
+//        final Button button = (Button) findViewById(R.id.addTeacher);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent addTeacherIntent = new Intent(HomeActivity.this, AddTeacherActivity.class);
+//                HomeActivity.this.startActivity(addTeacherIntent);
+//            }
+//        });
 
         // Displays a alert window and lets you know if your DB connection is successful.
+        // If student data is returned, then the connection was successful.
         getDBConnectionAlert();
 
     }
