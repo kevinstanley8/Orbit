34f98c42aa9e5eb781f10f7213f5b19d8aefd6ea
Brock Tubre <brock.tubre@csra.com>
2018-02-04 09:26:17 -0600

    Simple list of courses associated with Teacher logged in

--- Source/Orbit/app/src/main/java/net/orbit/orbit/activities/ViewCoursesTeacherActivity.java
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/activities/ViewCoursesTeacherActivity.java
@@ -1,12 +1,17 @@
 package net.orbit.orbit.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +20,11 @@ import net.orbit.orbit.models.pojo.Course;
 import net.orbit.orbit.R;
 import net.orbit.orbit.services.CourseService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewCoursesTeacherActivity extends BaseActivity {
-
+    private RecyclerView recyclerView;
     CourseService courseService = new CourseService(this);
 
     public static Intent createIntent(Context context) {
@@ -37,8 +43,6 @@ public class ViewCoursesTeacherActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        courseService.getAllCourses(this);
-
 
         FloatingActionButton mFabAddCourse = (FloatingActionButton) findViewById(R.id.fab_add_course);
         mFabAddCourse.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +53,19 @@ public class ViewCoursesTeacherActivity extends BaseActivity {
             }
 
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new Adapter(this));
+
+        courseService.getAllCourses(this);
+
     }
 
     public void updateCourseList(List<Course> courseList){
 
 
-        if(courseList.size() > 0){
+        if(courseList.size() == 0){
             Log.i("ViewCourseActivity", "No courses found for teacher logged in.");
             Toast.makeText(this, "You have no courses." , Toast.LENGTH_SHORT).show();
             TextView noCoursesFound = (TextView)findViewById(R.id.noCoursesFound);
@@ -63,11 +74,102 @@ public class ViewCoursesTeacherActivity extends BaseActivity {
         }
 
         Log.i("ViewCourseActivity", "Found courses associated with teacher logged in." + courseList);
-        
+        for (Course c : courseList){
+            Adapter.courses.add(c);
+        }
+        reloadList();
+    }
 
+    public void reloadList()
+    {
+        recyclerView.getAdapter().notifyDataSetChanged();
+    }
 
+    public static class Adapter extends RecyclerView.Adapter<ViewCoursesTeacherActivity.ViewHolder> {
+
+        private final Activity context;
+        private static List<Course> courses = new ArrayList<>();
+
+        public Adapter(Activity context) {
+            this.context = context;
+        }
+
+        public static void addCourse(Course course)
+        {
+            courses.add(course);
+        }
+
+        @Override
+        public ViewCoursesTeacherActivity.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = context.getLayoutInflater().inflate(R.layout.course_item, parent, false);
+            return new ViewCoursesTeacherActivity.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewCoursesTeacherActivity.ViewHolder holder, int position) {
+            Course course = courses.get(position);
+
+            holder.txtCourseName.setText(course.getName());
+
+            //set created text info section
+            StringBuilder sb = new StringBuilder();
+
+            //String testImage = "http://media2.s-nbcnews.com/j/streams/2013/june/130617/6c7911377-tdy-130617-leo-toasts-1.nbcnews-ux-2880-1000.jpg";
+            //Glide.with(context).load(testImage).into(holder.memeImage);
+        }
+
+        @Override
+        public int getItemCount() {
+            return courses.size();
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        public final ImageView iconImage;
+        public final TextView txtCourseName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
+            iconImage = (ImageView) itemView.findViewById(R.drawable.default_course_icon_black);
+            txtCourseName = (TextView) itemView.findViewById(R.id.txtCourseName);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            /*TextView top = (TextView) itemView.findViewById(R.id.txtTop);
+            TextView bottom = (TextView) itemView.findViewById(R.id.txtBottom);
+            TextView url = (TextView) itemView.findViewById(R.id.txtURLString);
+
+            String text = (String) top.getText();
+
+            Context context = itemView.getContext();
+            context.startActivity(MemeCloseUpActivity.createIntent(
+                    context, (String) top.getText(), (String) bottom.getText(), (String) url.getText()));*/
+
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            /*int position = getAdapterPosition();
+            String top = Adapter.memes.get(position).getTxtTop();
+            String bottom = Adapter.memes.get(position).getTxtBottom();
+            String url = Adapter.memes.get(position).getMemeURL();
+
+            Context context = itemView.getContext();
+            int TEST = 0;
+
+            context.startActivity(EditMeme.createIntent(
+                    context, Adapter.memes, position, itemView, top, bottom, url));*/
+
+            return false;
+        }
 
     }
+
 
 
 }
