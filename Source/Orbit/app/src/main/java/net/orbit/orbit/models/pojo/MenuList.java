438764cb5e02b1d3fa4d22482697911deb053953
brocktubre <brocktubre@gmail.com>
2018-02-12 19:44:04 -0600

    Changed all the icons

--- Source/Orbit/app/src/main/java/net/orbit/orbit/models/pojo/MenuList.java
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/models/pojo/MenuList.java
@@ -13,30 +13,30 @@ import java.util.List;
 public class MenuList
 {
     public static final List<MainMenuItem> adminMenuList = Arrays.asList(
-            new MainMenuItem(R.string.menu_add_teacher, R.string.menu_add_teacher, R.drawable.menu_teacher),
-            new MainMenuItem(R.string.menu_add_student,R.string.menu_add_student, R.drawable.menu_student),
-            new MainMenuItem(R.string.menu_enroll_student_in_course, R.string.menu_enroll_student_in_course, R.drawable.menu_enroll_student_in_course),
-            new MainMenuItem(R.string.menu_choose_student, R.string.menu_choose_student, R.drawable.menu_choose_student),
+            new MainMenuItem(R.string.menu_add_teacher, R.string.menu_add_teacher, R.drawable.ic_menu_add_teacher),
+            new MainMenuItem(R.string.menu_add_student,R.string.menu_add_student, R.drawable.ic_menu_add_student),
+            new MainMenuItem(R.string.menu_enroll_student_in_course, R.string.menu_enroll_student_in_course, R.drawable.ic_menu_enroll_student_in_course),
+            new MainMenuItem(R.string.menu_choose_student, R.string.menu_choose_student, R.drawable.ic_menu_choose_student),
             new MainMenuItem(R.string.menu_link_student,R.string.menu_link_student, R.drawable.menu_link_parent_student),
             new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.menu_logout));
 
     public static final List<MainMenuItem> teacherMenuList = Arrays.asList(
-            new MainMenuItem(R.string.menu_add_student,R.string.menu_add_student, R.drawable.menu_student),
-            new MainMenuItem(R.string.menu_enroll_student_in_course, R.string.menu_enroll_student_in_course, R.drawable.menu_enroll_student_in_course),
-            new MainMenuItem(R.string.choose_course,R.string.choose_course, R.drawable.menu_link_parent_student),
-            new MainMenuItem(R.string.view_course,R.string.view_course, R.drawable.menu_link_parent_student),
-            new MainMenuItem(R.string.view_assignments,R.string.view_assignments, R.drawable.menu_link_parent_student),
-            new MainMenuItem(R.string.create_assignment,R.string.create_assignment, R.drawable.menu_link_parent_student),
-            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.menu_logout));
+            new MainMenuItem(R.string.menu_add_student,R.string.menu_add_student, R.drawable.ic_menu_add_student),
+            new MainMenuItem(R.string.menu_enroll_student_in_course, R.string.menu_enroll_student_in_course, R.drawable.ic_menu_enroll_student_in_course),
+            new MainMenuItem(R.string.choose_course,R.string.choose_course, R.drawable.ic_menu_choose_course),
+            new MainMenuItem(R.string.view_course,R.string.view_course, R.drawable.ic_menu_view_courses),
+            new MainMenuItem(R.string.view_assignments,R.string.view_assignments, R.drawable.ic_menu_view_assignment),
+            new MainMenuItem(R.string.create_assignment,R.string.create_assignment, R.drawable.ic_menu_create_assignment),
+            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.ic_menu_logout));
 
     public static final List<MainMenuItem> parentMenuList = Arrays.asList(
-            new MainMenuItem(R.string.menu_choose_student, R.string.menu_choose_student, R.drawable.menu_choose_student),
+            new MainMenuItem(R.string.menu_choose_student, R.string.menu_choose_student, R.drawable.ic_menu_choose_student),
             new MainMenuItem(R.string.menu_link_student,R.string.menu_link_student, R.drawable.menu_link_parent_student),
-            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.menu_logout));
+            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.ic_menu_logout));
 
 
     public static final List<MainMenuItem> studentMenuList = Arrays.asList(
-            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.menu_logout));
+            new MainMenuItem(R.string.menu_logout, R.string.menu_logout, R.drawable.ic_menu_logout));
 
 
 }
