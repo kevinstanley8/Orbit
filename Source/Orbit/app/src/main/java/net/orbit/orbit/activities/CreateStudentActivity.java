b6470bfd5e19cf936eaef0ee3c9f29bc02369f43
brocktubre <brocktubre@gmail.com>
2018-02-19 19:58:12 -0600

    Added a date picker

--- Source/Orbit/app/src/main/java/net/orbit/orbit/activities/CreateStudentActivity.java
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/activities/CreateStudentActivity.java
@@ -1,18 +1,29 @@
 package net.orbit.orbit.activities;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import net.orbit.orbit.models.pojo.Student;
 import net.orbit.orbit.R;
 import net.orbit.orbit.services.StudentService;
 import net.orbit.orbit.utils.Constants;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by David on 11/8/2017.
@@ -21,6 +32,7 @@ import net.orbit.orbit.utils.Constants;
 public class CreateStudentActivity extends BaseActivity
 {
     private StudentService studentService = new StudentService(this);
+    private int mYear,mMonth,mDay;
 
     public static Intent createIntent(Context context) {
         Intent i = new Intent(context, CreateStudentActivity.class);
@@ -40,13 +52,13 @@ public class CreateStudentActivity extends BaseActivity
 
         final EditText studentFirstName = (EditText) findViewById(R.id.studentFirstName);
         final EditText studentLastName = (EditText) findViewById(R.id.studentLastName);
-        final EditText studentDOB = (EditText) findViewById(R.id.studentDateOfBirth);
         final EditText studentSSN = (EditText) findViewById(R.id.studentSSN);
         final EditText studentAddress1 = (EditText) findViewById(R.id.studentAddress1);
         final EditText studentAddress2 = (EditText) findViewById(R.id.studentAddress2);
         final EditText studentCity = (EditText) findViewById(R.id.studentCity);
         final EditText studentState = (EditText) findViewById(R.id.studentState);
         final EditText studentZipCode = (EditText) findViewById(R.id.studentZip);
+        final TextView dateTextView = (TextView) findViewById(R.id.studentDateOfBirth);
         final String studentGrade = Constants.FILLOUT_LATER;
 
         final String motherFirstName = Constants.FILLOUT_LATER;
@@ -89,7 +101,7 @@ public class CreateStudentActivity extends BaseActivity
                 Student newStudent = new Student(
                                 studentFirstName.getText().toString(),
                                 studentLastName.getText().toString(),
-                                studentDOB.getText().toString(),
+                                dateTextView.getText().toString(),
                                 studentSSN.getText().toString(),
                                 studentAddress1.getText().toString(),
                                 studentAddress2.getText().toString(),
@@ -126,5 +138,80 @@ public class CreateStudentActivity extends BaseActivity
 
             }
         });
+
+        dateTextView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    // Initialize a new date picker dialog fragment
+                    DialogFragment dFragment = new RegisterActivity.DatePickerFragment();
+
+                    // Show the date picker dialog fragment
+                    dFragment.show(getFragmentManager(), "Date Picker");
+                    return true;
+                }
+                return false;
+
+            }
+        });
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            /*
+                We should use THEME_HOLO_LIGHT, THEME_HOLO_DARK or THEME_TRADITIONAL only.
+
+                The THEME_DEVICE_DEFAULT_LIGHT and THEME_DEVICE_DEFAULT_DARK does not work
+                perfectly. This two theme set disable color of disabled dates but users can
+                select the disabled dates also.
+
+                Other three themes act perfectly after defined enabled date range of date picker.
+                Those theme completely hide the disable dates from date picker object.
+             */
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_HOLO_LIGHT,this,year,month,day);
+
+            // Add 3 days to Calendar
+            calendar.add(Calendar.DATE, 1);
+
+
+            // Set the Calendar new date as maximum date of date picker
+            dpd.getDatePicker().setMaxDate(calendar.getTimeInMillis());
+
+            // Subtract 6 days from Calendar updated date
+            calendar.add(Calendar.DATE, -10000);
+
+            // Set the Calendar new date as minimum date of date picker
+            dpd.getDatePicker().setMinDate(calendar.getTimeInMillis());
+
+            // Return the DatePickerDialog
+            return  dpd;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+            // Do something with the chosen date
+            TextView date = (TextView) getActivity().findViewById(R.id.studentDateOfBirth);
+
+            // Create a Date variable/object with user chosen date
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(0);
+            cal.set(year, month, day, 0, 0, 0);
+            Date chosenDate = cal.getTime();
+
+            // Format the date using style and locale
+            String formattedDate = new SimpleDateFormat(Constants.DATE_FORMAT).format(chosenDate);
+
+            // Display the chosen date to app interface
+            date.setText(formattedDate);
+        }
     }
 }
