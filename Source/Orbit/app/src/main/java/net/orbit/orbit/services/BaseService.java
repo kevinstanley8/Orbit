c332e3a5da6c0f3ece341f447bd058577ed1959b
brocktubre <brocktubre@gmail.com>
2018-02-22 21:43:30 -0600

    Properties working correctly.

--- Source/Orbit/app/src/main/java/net/orbit/orbit/services/BaseService.java
+++ Source/Orbit/app/src/main/java/net/orbit/orbit/services/BaseService.java
@@ -14,7 +14,6 @@ import net.orbit.orbit.utils.PropertiesService;
 public class BaseService {
     private PropertiesService propertiesService;
     private OrbitRestClient orbitRestClient;
-    private OrbitUserPreferences orbitUserPreferences;
 
     public PropertiesService getPropertiesService(Context context){
         if(this.propertiesService == null){
@@ -26,7 +25,7 @@ public class BaseService {
     public OrbitRestClient getOrbitRestClient(Context context){
         if(this.orbitRestClient == null){
             this.orbitRestClient = new OrbitRestClient(context);
-            this.orbitRestClient.setBaseUrl(this.propertiesService.getProperty(context, Constants.ORBIT_API_URL));
+            this.orbitRestClient.setBaseUrl(getPropertiesService(context).getProperty(context, Constants.ORBIT_API_URL));
             return this.orbitRestClient;
         }
         return this.orbitRestClient;
